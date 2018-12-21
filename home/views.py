299b32029b7ee54ee342851413bb9e5b2fbd9f59
SM Mashuque <smjim1998@gmail.com>
2018-12-20 20:57:05 -0500

    add the home page view

---
+++ home/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+from django.views import generic
+
+
+class HomePageView(generic.TemplateView):
+    template_name = 'home_page.html'
+
