787b1a4d133282400b8b9b3f1b7e573362f4515f
SM Mashuque <smjim1998@gmail.com>
2018-12-31 19:26:45 -0500

    create celery task to load data from cornell dining endpoint.

--- cu_food_reviews/tasks.py
+++ cu_food_reviews/tasks.py
@@ -1,5 +1,10 @@
 from __future__ import absolute_import, unicode_literals
+from django.core.management import call_command
 from .celery import app
+
+@app.task
+def load_data_from_endpoint_task():
+    call_command('load_data_from_endpoint')
 
 @app.task
 def add(x, y):
@@ -10,3 +15,4 @@ def add(x, y):
 def fib(n):
     if n <= 1: return n
     return fib(n-2) + fib(n-1)
+
