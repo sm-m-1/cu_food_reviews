4da4e0264c6d3332822f7f8b29245356145fc99e
SM Mashuque <smjim1998@gmail.com>
2019-01-15 04:38:27 -0500

    break up the long view function.

--- locations/views.py
+++ locations/views.py
@@ -19,7 +19,6 @@ class LocationList(ListView):
         today = self.request.GET.get('date', date.today().isoformat())
         open_today = self.request.GET.get('open_today')
 
-        object_list_new = []
 
         location_qs = self.get_queryset().prefetch_related('operatinghour_set', 'operatinghour_set__mealevent_set')
         event_qs = MealEvent.objects.filter(operating_hour__date=today).select_related('operating_hour', 'operating_hour__location')
@@ -36,54 +35,68 @@ class LocationList(ListView):
             avg_rating=Avg('review__rating')
         )
 
+        object_list_new = []
         for location in location_qs:
-            info = {
-                'location': location,
-                'location_data': [],
-                'dining_items': [],
-            }
-            meal_events = [ event for event in event_qs if event.operating_hour.location == location ]
-
-            if open_today == 'on' and len(meal_events) == 0: continue  # skip location that is closed today
-
-            for event in meal_events:
-                meal_categories = [ category for category in category_qs if category.meal_event == event ]
-                meal_category_data = []
-                for category in meal_categories:
-                    data = {
-                        'category': category,
-                        'category_items': []
-                    }
-                    for meal_item in item_qs:
-                        category_qs = meal_item.meal_category.all()
-                        if meal_item.meal_location == location:
-                            for c in category_qs:
-                                if c.meal_event == event and c == category:
-                                    data['category_items'].append(meal_item)
-                                    break
-
-                    meal_category_data.append(data)
-
-                data = {
-                    'event': event,
-                    'meal_category_data': meal_category_data
-                }
-                info['location_data'].append(data)
-
-                dining_items = []
-                for meal_item in item_qs:
-                    if meal_item.meal_location == location and meal_item.is_dining_item == True:
-                        dining_items.append(meal_item)
-                info['dining_items'] = dining_items
-
-            object_list_new.append(info)
+            location_info = self.build_location_data(location, event_qs, category_qs, item_qs, open_today)
+            if location_info: object_list_new.append(location_info)
 
         context['object_list'] = object_list_new
-        next_seven_days = [( date.today() + timedelta(days=i) ).isoformat() for i in range(7)]
-        context['date_list'] = next_seven_days
-
+        context['date_list'] = [( date.today() + timedelta(days=i) ).isoformat() for i in range(7)]
         return context
 
+    def build_location_data(self, location, event_qs=None, category_qs=None, item_qs=None, open_today='on'):
+        """
+        Given a location, this method builds the location info for it.
+        :return: A list
+        """
+        info = {
+            'location': location,
+            'location_data': [],
+            'dining_items': [],
+        }
+        meal_events = [event for event in event_qs if event.operating_hour.location == location]
+
+        if open_today == 'on' and len(meal_events) == 0: return None # skip location that is closed today
+
+        for event in meal_events:
+            meal_categories = [category for category in category_qs if category.meal_event == event]
+            meal_category_data = self.build_location_category_data(location, event, meal_categories, item_qs)
+
+            data = {
+                'event': event,
+                'meal_category_data': meal_category_data
+            }
+            info['location_data'].append(data)
+
+            dining_items = []
+            for meal_item in item_qs:
+                if meal_item.meal_location == location and meal_item.is_dining_item == True:
+                    dining_items.append(meal_item)
+            info['dining_items'] = dining_items
+
+        return info
+
+    def build_location_category_data(self, location, event=None, meal_categories=None, item_qs=None):
+        """
+        For a given location. Build the meal category data for it.
+        :return: a list
+        """
+        meal_category_data = []
+        for category in meal_categories:
+            data = {
+                'category': category,
+                'category_items': []
+            }
+            for meal_item in item_qs:
+                category_qs = meal_item.meal_category.all()
+                if meal_item.meal_location == location:
+                    for c in category_qs:
+                        if c.meal_event == event and c == category:
+                            data['category_items'].append(meal_item)
+                            break
+
+            meal_category_data.append(data)
+        return meal_category_data
 
 
     def get_queryset(self):
